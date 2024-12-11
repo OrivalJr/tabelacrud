bbeb5e02d912598cdf2d92d9cd667badc0d05197
Orival de Siqueira Junior <orivaljr.2014@gmail.com>
2024-12-11 19:02:02 -0300

    graphql generated

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "tabelacrud": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
