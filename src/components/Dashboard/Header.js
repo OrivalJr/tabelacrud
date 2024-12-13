9942cc60bf2764770be43153a850392fe2230f28
Orival de Siqueira Junior <orivaljr.2014@gmail.com>
2024-12-12 22:58:56 -0300

    header

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import Logout from '../Logout';
+import { Button } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 const Header = ({ setIsAdding, setIsAuthenticated }) => {
   return (
     <header>
       <h1>Gestão de Condomínios</h1>
       <div style={{ marginTop: '30px', marginBottom: '18px' }}>
-        <button onClick={() => setIsAdding(true)}>Add Employee</button>
+        <Button onClick={() => setIsAdding(true)}>Add Employee</Button>
+        <Button>Botao1</Button>
         <Logout setIsAuthenticated={setIsAuthenticated} />
       </div>
     </header>
